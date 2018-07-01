94796e35c66ede3ebe63118d4596130cab22b024
ZaherAirout <m.zaher.airout@gmail.com>
2018-07-01 15:00:09 +0300

    update

--- default_config.py
+++ default_config.py
@@ -1,6 +1,6 @@
 class DefaultConfig:
     u_name = 'guest'
-    server_url = '192.168.43.243'
+    server_url = '192.168.1.11'
     connection_type = 'socket'
     MaxImagesCount = 5
     FACE_RECOGNITION_PORT = 8888
