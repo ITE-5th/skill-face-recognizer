e514e76e1d0ca8be01a3bf03841073da1f45dabd
ZaherAirout <m.zaher.airout@gmail.com>
2018-07-01 14:25:08 +0300

    update

--- __init__.py
+++ __init__.py
@@ -49,7 +49,7 @@ class FaceRecognizerSkill(MycroftSkill):
         # self.new_person = None
         # self.connect()
 
-    @intent_handler(IntentBuilder("RecognizeIntent").require('Play'))
+    @intent_handler(IntentBuilder("RecognizeIntent").require('Face'))
     def handle_recognize_intent(self):
         # try:
         #     image, _ = self.camera.take_image()
