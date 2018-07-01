8f47bff1d96f74885160ca04b2f69661c4eca231
ZaherAirout <m.zaher.airout@gmail.com>
2018-07-01 11:05:59 +0300

    update

--- __init__.py
+++ __init__.py
@@ -9,8 +9,12 @@ from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
 
+from .code.message.add_person_message import AddPersonMessage
 from .code.message.face_recognition_message import FaceRecognitionMessage
+from .code.message.register_face_recognition_message import RegisterFaceRecognitionMessage
 from .code.misc.camera import Camera
+from .code.misc.http.api import get_http_request_type
+from .code.misc.http.api import request_http
 from .code.misc.receiver import Receiver
 from .code.misc.sender import Sender
 from .default_config import DefaultConfig
@@ -36,27 +40,65 @@ class FaceRecognizerSkill(MycroftSkill):
 
         if "server_url" not in self.settings:
             self.settings["server_url"] = DefaultConfig.server_url
+        self.name = None
         self.socket = None
         self.receiver = None
         self.sender = None
         self.port = None
         self.host = None
         self.camera = Camera(width=800, height=600)
-        self.connect()
+        self.connection_type = DefaultConfig.connection_type
+        self.registered = False
+        self.new_person = None
 
     def connect(self):
         try:
-            self.port = DefaultConfig.FACE_RECOGNITION_PORT
+            self.connection_type = self.settings.get("connection_type", DefaultConfig.connection_type)
             # self.host = self.settings.get("server_url", DefaultConfig.server_url)
+            LOG.info('settings server : ' + self.settings.get("server_url"))
             self.host = DefaultConfig.server_url
-            LOG.info("Face Recognizer Skill started " + self.host + ":" + str(self.port))
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.connect((self.host, self.port))
-            self.receiver = Receiver(self.socket, json=True)
-            self.sender = Sender(self.socket, json=True)
-            LOG.info('connected to server:' + self.host + ' : ' + str(self.port))
+            self.port = DefaultConfig.FACE_RECOGNITION_PORT
+            if self.connection_type == 'socket':
+                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                LOG.info('connecting to server:' + self.host + ' : ' + str(self.port))
+                self.socket.connect((self.host, self.port))
+                self.receiver = Receiver(self.socket, json=True)
+                self.sender = Sender(self.socket, json=True)
+                #
+                LOG.info('connected to server:' + self.host + ' : ' + str(self.port))
+
         except Exception as e:
             LOG.warning(str(e))
+
+    def send_recv(self, msg, user_name=DefaultConfig.name, target_name=None):
+        if self.connection_type == 'http':
+            url, method = get_http_request_type(msg, user_name, target_name)
+            url = self.host + url
+            return request_http(url, method, msg)
+        else:
+            sent = self.ensure_send(msg)
+            if not sent:
+                return None
+            result = self.receiver.receive()
+            return result
+
+    def register_face(self):
+        LOG.info("register face")
+        if self.registered:
+            return True
+
+        self.name = self.settings.get('name', DefaultConfig.name)
+
+        msg = RegisterFaceRecognitionMessage(self.name)
+        result = self.send_recv(msg)
+        if not result:
+            self.speak_dialog('RegisterError')
+            return False
+
+        LOG.info(result)
+        self.speak_dialog("AddResult", self.new_person)
+        self.registered = True
+        return True
 
     def ensure_send(self, msg):
         retries = 3
@@ -98,11 +140,49 @@ class FaceRecognizerSkill(MycroftSkill):
             return False
         return True
 
+    @intent_handler(IntentBuilder("FaceIntent").require('add').require('name'))
+    def add(self, message):
+        if not self.registered:
+            registered = self.register_face()
+            if not registered:
+                return False
+        LOG.info(message.data)
+        self.new_person = message.data.get('name')
+        return True
+
+    @intent_handler(IntentBuilder("FaceIntent").require('capture'))
+    def capture(self, message):
+        if not self.registered:
+            registered = self.register_face()
+            if not registered:
+                return False
+        if self.new_person is None:
+            self.speak('Please add person before capture')
+            return True
+        image, _ = self.camera.take_image(1)
+        if image is None:
+            self.speak_dialog("PersonCountError")
+            return True
+        msg = AddPersonMessage(image)
+        # sent = self.ensure_send(msg)
+        result = self.send_recv(msg)
+        if not result:
+            return False
+        # result = self.receiver.receive()
+        LOG.info(result)
+        self.speak_dialog("AddResult")
+
+        self.images_count += self.images_count
+        if self.images_count > DefaultConfig.MaxImagesCount:
+            self.new_person = None
+
+        return True
+
     @staticmethod
     def handle_message(response):
         """
         converts server response to meaningful sentence
-        :param response: string of answers
+        :param response: string of people names includes unknown
         :return: dictionary contains sentence in result
         """
         unknown = 'Unknown'
