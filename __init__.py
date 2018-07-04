b86b4e24371b68d39423e8c10fb90c19a09c99f8
ZaherAirout <m.zaher.airout@gmail.com>
2018-07-04 09:33:14 +0300

    update

--- __init__.py
+++ __init__.py
@@ -127,7 +127,7 @@ class FaceRecognizerSkill(MycroftSkill):
     @intent_handler(IntentBuilder("AddFaceIntent").require('Add'))
     def add(self, message = ''):
         p_name = self.get_person_name()
-        self.speak_dialog("AddPresonStart.dialog", {'p_name': p_name})
+        self.speak_dialog("AddPeronStart", {'p_name': p_name})
         self.new_person = p_name
         return True
 
